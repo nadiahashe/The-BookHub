1100cbdebd5a98fd024c48854ae52e9626749221
bcline01 <brookecline.bc@gmail.com>
2024-11-12 22:18:32 -0500

    landing page is mobile responsive

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -11,15 +11,15 @@ export default function Landing() {
           <img src={logo} alt="Logo" className="logo" />
           <h1 className="header-text">The Book Hub</h1>
         </div>
+        <NavLink to="/login" className="cta-button btn btn-primary">LogIn</NavLink>
       </header>
       <section id="hero">
         <div className="hero-content container-fluid" style={{ backgroundColor: "#faefe0", height: "100vh" }}>
           <div className="row h-100">
             {/* Left Column: Text */}
-            <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <div className="col-md-6 d-flex ">
               <div className="hero-text">
                 <h1>Where Stories Connect Us All</h1>
-                <NavLink to="/login" className="cta-button btn btn-primary">LogIn</NavLink>
               </div>
             </div>
 
