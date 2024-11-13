2f8ffe72e64c333e1397ece425bd8c9dc97cb5c2
kc-claas <keithclaas@gmail.com>
2024-11-12 20:40:41 -0600

    changed username to string and added public to review model

--- server/src/models/review.ts
+++ server/src/models/review.ts
@@ -1,11 +1,12 @@
 // A review is a contains content and userID
 
-import { Schema, model, Document, ObjectId } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
 interface IReview extends Document {
     content: string,
-    username: ObjectId,
-    progress: string
+    username: string,
+    progress: string,
+    public: boolean
 }
 
 const reviewSchema = new Schema<IReview>(
@@ -14,11 +15,14 @@ const reviewSchema = new Schema<IReview>(
             type: String,
         },
         username: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
+            type: String,
+            required: true
         },
         progress: {
             type: String
+        },
+        public: {
+            type: Boolean
         }
     }  
 )
