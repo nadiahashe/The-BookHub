ce44416d528ae810879aede485ce079cb22d959b
Nadia Hashemi <nadiahashemi@Nadias-MacBook-Pro-2.local>
2024-11-18 20:40:23 -0500

    cypress component setup

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  // e2e: {
+  //   setupNodeEvents(on, config) {
+  //     // implement node event listeners here
+  //   },
+  //   supportFile: "cypress/support/e2e.ts", // Path to your support file
+  //   specPattern: "cypress/e2e/**/*.ts", // Path to your TypeScript test files
+  // },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "cypress/component/**/*.{ts,tsx}"
+  },
+});
